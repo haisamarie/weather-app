4170a6158e915c7f318694386a2091fd8700f7f4
haisamarie <marie.199204@gmail.com>
2025-07-09 21:27:25 +0900

    都市名から緯度、経度を取得する関数を追加し、APIからのデータ取得を実装

--- src/App.tsx
+++ src/App.tsx
@@ -19,33 +19,49 @@ const TEST_DATA: data[] = [
   },
 ];
 
-function App() {
+//都市名から緯度、経度を取得する関数。
+const useGetCity = (city: string) => {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
-  const lat = 35.6895; // 東京の緯度
-  const lon = 139.6917; // 東京の経度
-  const { data, error, isLoading } = useSWR(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`,
-    fetcher
-  );
 
-  const [city, setCity] = useState("");
+  const key = city
+    ? `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+    : null;
+
+  const { data, error, isLoading } = useSWR(key, fetcher);
+  return {
+    cityList: data,
+    isLoading,
+    isError: Boolean(error),
+  };
+};
+
+function App() {
+  const [inputCity, setInputCity] = useState("");
+  const [searchCity, setSearchCity] = useState("");
   const [weather, setWeather] = useState(TEST_DATA);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCity(e.target.value);
-  };
+  const { cityList, isLoading, isError } = useGetCity(searchCity);
 
-  console.log(data, "data");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputCity(e.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSearchCity(inputCity.trim());
+  };
 
   return (
     <div className="App">
       <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-100 to-blue-200">
         <div className="w-full max-w-sm mb-6">
-          <form className="flex shadow-md rounded-full overflow-hidden bg-white">
+          <form
+            onSubmit={handleSubmit}
+            className="flex shadow-md rounded-full overflow-hidden bg-white"
+          >
             <input
               onChange={handleChange}
-              value={city}
+              value={inputCity}
               type="text"
               placeholder="都市名を入力"
               className="flex-1 px-5 py-3 bg-transparent focus:outline-none text-gray-700 placeholder:text-gray-400"
