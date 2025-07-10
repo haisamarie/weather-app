faf3f5d8f1d0d8a33e6e88d8f182ec2de20d2a7c
haisamarie <marie.199204@gmail.com>
2025-07-10 19:04:36 +0900

    プレースホルダーとエラーメッセージを英語に変更し、天気情報の表示部分を整理

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
               onChange={handleChange}
               value={inputCity}
               type="text"
-              placeholder="都市名を入力"
+              placeholder="Enter city name"
               className="flex-1 px-5 py-3 bg-transparent focus:outline-none text-gray-700 placeholder:text-gray-400"
             />
             <button
@@ -96,36 +96,38 @@ function App() {
               type="submit"
               className="bg-blue-300 text-white px-7 py-3 font-bold hover:bg-blue-400 transition"
             >
-              検索
+              search
             </button>
           </form>
         </div>
 
         {/* 天気取得状態 */}
-        {weatherLoading && <p>天気情報を取得中…</p>}
-        {weatherError && <p>天気情報の取得に失敗しました</p>}
-
-        <div className="bg-white rounded-3xl shadow-xl px-10 py-8 flex flex-col items-center w-full max-w-xs">
-          <div className="text-lg font-bold text-blue-600 mb-2">
-            {weatherData?.name}
-          </div>
-          {weatherData?.weather.map((item) => (
-            <div key={item.id}>
-              <img
-                src={iconUrl}
-                alt={weatherData.weather[0].description}
-                width={100}
-                height={100}
-              />
-              <div className="text-4xl font-bold text-gray-700 mb-2">
-                {weatherData.main.temp.toFixed(1)}°C
-              </div>
-              <div className="text-base text-gray-500 tracking-wide text-center">
-                {item.main}
-              </div>
+        {weatherLoading && <p>Loading…</p>}
+        {weatherError && <p>Failed to get weather information</p>}
+        {/* 天気情報 */}
+        {weatherData && (
+          <div className="bg-white rounded-3xl shadow-xl px-10 py-8 flex flex-col items-center w-full max-w-xs">
+            <div className="text-lg font-bold text-blue-600 mb-2">
+              {weatherData?.name}
             </div>
-          ))}
-        </div>
+            {weatherData?.weather.map((item) => (
+              <div key={item.id}>
+                <img
+                  src={iconUrl}
+                  alt={weatherData.weather[0].description}
+                  width={100}
+                  height={100}
+                />
+                <div className="text-4xl font-bold text-gray-700 mb-2">
+                  {weatherData.main.temp.toFixed(1)}°C
+                </div>
+                <div className="text-base text-gray-500 tracking-wide text-center">
+                  {item.main}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
